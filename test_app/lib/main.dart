611af815ede3e6a5e3689c4c4fd58c85966828c5
WulfNB <wulfnb@gmail.com>
2021-06-03 22:02:55 +0530

    Creating Button

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -7,13 +7,21 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
           appBar: AppBar(
             title: Text("My firstTest App"),
           ),
-          body: Text("Start"),
+          body: Column(
+            children: [
+              Text("What is your favourive colour"),
+              RaisedButton(child: Text("Answer 1"), onPressed: null,),
+              RaisedButton(child: Text("Answer 2"), onPressed: null,),
+              RaisedButton(child: Text("Answer 3"), onPressed: null,)
+            ],
+          ),
     ));
   }
 }
