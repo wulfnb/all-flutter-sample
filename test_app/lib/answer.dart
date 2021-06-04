a36e463971dc54d487bc7a6cd59b5f7bbd370176
WulfNB <wulfnb@gmail.com>
2021-06-04 07:41:41 +0530

    Added anwer file and callback function

---
+++ test_app/lib/answer.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+
+  final Function selectHandler;
+
+  Answer(this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        child: Text("Answer 1"), 
+        color: Colors.blue,
+        onPressed: () => {selectHandler()}),
+    );
+  }
+}
