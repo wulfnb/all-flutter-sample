50df331635059b9668e4bcec0559eed7fc13a905
WulfNB <wulfnb@gmail.com>
2021-06-04 06:26:49 +0530

    Added statefull widget

--- test_app/lib/main.dart
+++ test_app/lib/main.dart
@@ -3,12 +3,31 @@ import 'package:flutter/material.dart';
 // void main() {
 //   runApp(MyApp());
 // }
-
+// 23
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  var questionSelected = 0;
+
+  void answerQuestion(){
+    setState(() {
+      questionSelected = questionSelected + 1;
+    });
+    print(questionSelected);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What is your name ?",
+      "What is your favourive colour ?"
+    ];
     return MaterialApp(
         home: Scaffold(
           appBar: AppBar(
@@ -16,10 +35,10 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              Text("What is your favourive colour"),
-              RaisedButton(child: Text("Answer 1"), onPressed: null,),
-              RaisedButton(child: Text("Answer 2"), onPressed: null,),
-              RaisedButton(child: Text("Answer 3"), onPressed: null,)
+              Text(questions.elementAt(questionSelected)),
+              RaisedButton(child: Text("Answer 1"), onPressed: answerQuestion,),
+              RaisedButton(child: Text("Answer 2"), onPressed: answerQuestion,),
+              RaisedButton(child: Text("Answer 3"), onPressed: answerQuestion,)
             ],
           ),
     ));
